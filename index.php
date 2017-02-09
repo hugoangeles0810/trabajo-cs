fd9b9b581d964f9608f609f59631573908a405fa
Hugo Angeles <hugoangeles0810@gmail.com>
2017-02-08 23:35:37 -0500

    Frontend de vista pricipal

---
+++ index.php
@@ -0,0 +1,241 @@
+<!doctype html>
+<html class="no-js" lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Vuelos | Despegar.com Perú</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="shortcut icon" href="assets/img/favicon.ico">
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+  </head>
+  <body>
+    <!-- Header y navegación -->
+    <header>
+      <div class="header-main">
+        <div class="header-main-inner">
+          <a href="#">
+            <img src="assets/img/despegar.gif" alt="Despegar.com">
+          </a>
+        </div>
+      </div>
+      <nav>
+        <div class="nav-inner">
+          <ul class="list-unstyled list-inline">
+            <li class="active"><a href="#"><i class="glyphicon glyphicon-send"></i> Vuelos</a></li>
+            <li><a href="#"><i class="glyphicon glyphicon-home"></i> Hoteles</a></li>
+          </ul>
+        </div>
+        <div class="nav-precios-bajos">
+          <img src="assets/img/precios-bajos.png" alt="Precios bajos">
+        </div>
+      </nav>
+    </header>
+    <!-- Fin: Header y navegación -->
+
+    <!-- Main Content -->
+    <section class="container-fluid main-content">
+      <div class="row">
+
+        <!-- Formulario de vuelos -->
+        <div class="col-md-6 col-xs-12">
+          <div class="main-form">
+            <form action="
+            ">
+              <h3>Vuelos</h3>
+              <div class="row-fluid clearfix mt-14">
+                <div class="col-xs-6 ph-5">
+                  <div class="form-group">
+                    <label for="origen">Origen</label>
+                    <input id="origen" type="text" class="form-control" placeholder="Ingresa desde donde viajas">
+                  </div>
+                </div>
+                <div class="col-xs-6 ph-5">
+                  <div class="form-group">
+                    <label for="destino">Destino</label>
+                    <input id="destino" type="text" class="form-control" placeholder="Ingresa hasta dónde viajas">
+                  </div>
+                </div>
+              </div>
+              <div class="row-fluid clearfix">
+                <div class="col-xs-6 ph-5">
+                  <div class="form-group">
+                    <label>
+                      <input type="checkbox" name="fecha" value="y">
+                      Todavía no he decidido mis fechas
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div class="row-fluid clearfix">
+                <div class="col-xs-3 ph-5">
+                  <div class="form-group">
+                    <label for="partida">¿En que fecha?</label>
+                    <input type="date" class="form-control" placeholder="Partida">
+                  </div>
+                </div>
+                <div class="col-xs-3 ph-5">
+                  <div class="form-group">
+                    <label class="only-ida"><input type="checkbox" name="soloida"> Solo ida</label>
+                    <input type="date" class="form-control" placeholder="Regreso">
+                  </div>
+                </div>
+                <div class="col-xs-3 ph-5">
+                  <div class="form-group">
+                    <label id="adultos">Adultos</label>
+                    <select name="adultos" id="adultos" class="form-control">
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                      <option value="6">6</option>
+                      <option value="7">7</option>
+                      <option value="8">8</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="col-xs-3 ph-5">
+                  <div class="form-group">
+                    <label id="menores">Menores</label>
+                    <select name="menores" id="menores" class="form-control">
+                      <option value="1">0</option>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                      <option value="6">6</option>
+                      <option value="7">7</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+              <div class="row-fluid clearfix mt-14">
+                <div class="col-xs-3">
+                  <button type="submit" class="btn btn-info btn-search">Buscar</button>
+                </div>
+                <div class="col-xs-9 low-cost">
+                  <span>Precios bajos garantizados</span>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        <!-- Fin: Formulario de vuelos -->
+        
+        <!-- Banners publicitarios -->
+        <div class="col-md-3">
+          <img src="assets/img/cuzco.png" class="img-responsive banner" alt="Cuzco">
+        </div>
+        <div class="col-md-3">
+          <img src="assets/img/outlet.png" class="img-responsive banner" alt="Outlet">
+        </div>
+        <!-- Fin: Banners publicitarios -->
+      </div>
+
+      <!-- Vuelos recomendados -->
+      <div class="row mt-42">
+
+        <div class="col-md-4 col-xs-12">
+          <div class="card">
+            <div class="picture" style="background: url(assets/img/place-cuzco.jpg);">
+              <div class="overlay"></div>
+              <div class="place-text"><i class="glyphicon glyphicon-send"></i> Cuzco</div>
+            </div>
+            <div class="text-right description">
+              <span>desde</span>
+              <p>S/. <span class="price">250</span></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4 col-xs-12">
+          <div class="card">
+            <div class="picture" style="background: url(assets/img/place-cuzco.jpg);">
+              <div class="overlay"></div>
+              <div class="place-text"><i class="glyphicon glyphicon-send"></i> Cuzco</div>
+            </div>
+            <div class="text-right description">
+              <span>desde</span>
+              <p>S/. <span class="price">250</span></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4 col-xs-12">
+          <div class="card">
+            <div class="picture" style="background: url(assets/img/place-cuzco.jpg);">
+              <div class="overlay"></div>
+              <div class="place-text"><i class="glyphicon glyphicon-send"></i> Cuzco</div>
+            </div>
+            <div class="text-right description">
+              <span>desde</span>
+              <p>S/. <span class="price">250</span></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4 col-xs-12">
+          <div class="card">
+            <div class="picture" style="background: url(assets/img/place-cuzco.jpg);">
+              <div class="overlay"></div>
+              <div class="place-text"><i class="glyphicon glyphicon-send"></i> Cuzco</div>
+            </div>
+            <div class="text-right description">
+              <span>desde</span>
+              <p>S/. <span class="price">250</span></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4 col-xs-12">
+          <div class="card">
+            <div class="picture" style="background: url(assets/img/place-cuzco.jpg);">
+              <div class="overlay"></div>
+              <div class="place-text"><i class="glyphicon glyphicon-send"></i> Cuzco</div>
+            </div>
+            <div class="text-right description">
+              <span>desde</span>
+              <p>S/. <span class="price">250</span></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4 col-xs-12">
+          <div class="card">
+            <div class="picture" style="background: url(assets/img/place-cuzco.jpg);">
+              <div class="overlay"></div>
+              <div class="place-text"><i class="glyphicon glyphicon-send"></i> Cuzco</div>
+            </div>
+            <div class="text-right description">
+              <span>desde</span>
+              <p>S/. <span class="price">250</span></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Fin: Vuelos recomendados -->
+    </section>
+    <!-- Fin: Main Content -->
+
+    <footer>
+      <h4 class="text-center">Despegar.com</h4>
+      <div class="row-fluid clearfix">
+        <div class="col-md-4 col-md-offset-4 col-xs-12 text-justify">
+          Trabajo encargado para el curso de Cliente/Servidor de la Escuela de Ingeniería Informática, Facultad de Ingeniería Industrial de la Universidad Nacional de Piura. Las imágenes mostradas son usadas con fines educativos :D.
+        </div>
+        <div class="col-md-4 col-xs-12">
+          <p>Integrantes</p>
+          <ul>
+            <li>Angeles Chavez, Hugo</li>
+            <li>Correa Mantilla, Roberto Ayrton</li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  </body>
+</html>
